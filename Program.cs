ee7880733459b8561743527fec96d3273931a51f
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-10-25 20:32:09 +0530

    [VIGNESH M] ADDED: Generics Array To Find Max Value

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsTestProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 112, 334, 432, 555, 678 };
+            GenericMaximum<int> generic = new GenericMaximum<int>(arr);
+            generic.PrintMaxValue();
+        }
+    }
+}
